a3cfc4cea0547ca4a0f1366a53fe289f46df66b8
Daniel-PC\Daniel <dmcleite@hotmail.com>
2016-10-27 16:21:39 +0200

    initialized Project

---
+++ TournamentCalculator/MainViewModel.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+using System.IO;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Base;
+
+namespace TournamentCalculator
+{
+    public class MainViewModel : ViewModelBase
+    {
+		#region members
+		#endregion
+
+		#region ctors
+        public MainViewModel()
+        {
+        }
+		#endregion
+
+		#region properties
+		#endregion
+
+		#region public methods
+		#endregion
+
+		#region private methods
+		#endregion
+    }
+}
+
